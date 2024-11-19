e2167492a72949954dc46fa781559b0c1abcd61f
karuniaperjuangan <karuniaperjuangan@mail.ugm.ac.id>
2024-11-19 13:45:24 +0700

    fix .env

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,6 +7,6 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   define: {
-    "VITE_BASE_API_URL": process.env.VITE_BASE_API_URL,
+    "VITE_BASE_API_URL": `${process.env.VITE_BASE_API_URL}`,
   },
 });
