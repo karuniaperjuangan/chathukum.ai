a07c8d317005e5bcfb90a8e9496215b7bdbaf0f6
karuniaperjuangan <karuniaperjuangan@mail.ugm.ac.id>
2024-11-19 13:16:07 +0700

    fix: frontend .env

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,11 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
-export default defineConfig(() => {
-  return {
-    plugins: [react()],
-    define: {
-      __APP_ENV__: process.env.VITE_BASE_API_URL,
-    },
-  };
+
+
+export default defineConfig({
+  plugins: [react()],
+  define: {
+    "process.env": process.env,
+    "VITE_BASE_API_URL": process.env.VITE_BASE_API_URL,
+  },
 });
